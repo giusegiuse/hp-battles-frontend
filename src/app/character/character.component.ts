1409f03eaeadb9b1086e251d2cb4185bbd6ffc1c
Giuseppe <giuseppe.sergi@beautyprof.com>
2023-11-22 09:11:10 +0100

    [WIP] Character component

--- src/app/character/character.component.ts
+++ src/app/character/character.component.ts
@@ -15,7 +15,7 @@ export class CharacterComponent implements OnInit, AfterViewInit {
 
   characters: Character[] = []
   dataSubscription?: Subscription
-
+  selectedCharacters: number[] = [];
 
   constructor(
     private route: ActivatedRoute,
@@ -32,15 +32,36 @@ export class CharacterComponent implements OnInit, AfterViewInit {
     this.startAnimation();
   }
 
+  getCardBackground(character: Character): string {
+    return character.faction === 'good' ? 'url("https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/hogwarts-gryffindor-pattern-7-black-gryphon.jpg")' :
+      'url("https://render.fineartamerica.com/images/rendered/default/print/8/8/break/images/artworkimages/medium/3/hogwarts-slytherin-pattern-3-black-gryphon.jpg")';
+  }
+
+  getCardHeaderBackground(character: Character): string {
+    return character.faction === 'good' ? 'linear-gradient(to bottom right, #ffd700, #b8860b)' : 'darkgreen'
+  }
 
   startAnimation() {
     if(this.characterCards)
     this.characterCards.forEach((element, index) => {
       setTimeout(() => {
         this.renderer.addClass(element.nativeElement, 'start-animation');
+        this.renderer.addClass(element.nativeElement, 'flipped');
       }, 100 * index);
     });
   }
 
+  toggleSelection(index: number) {
+    const selectedIndex = this.selectedCharacters.indexOf(index);
+    if (selectedIndex === -1) {
+      this.selectedCharacters.push(index);
+    } else {
+      this.selectedCharacters.splice(selectedIndex, 1);
+    }
+  }
+
+  isSelected(index: number): boolean {
+    return this.selectedCharacters.includes(index);
+  }
 
 }
